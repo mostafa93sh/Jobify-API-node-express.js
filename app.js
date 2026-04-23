d20dffac726f94c1f8ec28f993fa33943c719b70
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-23 22:15:12 +0200

    feat(app): integrate auth and jobs routers
    - Added auth router to /api/v1/auth
    - Added jobs router to /api/v1/jobs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -15,6 +17,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("jobs api");
 });
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
