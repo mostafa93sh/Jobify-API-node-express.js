4149cd05a477801251678980677688a6b67ab3c3
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-24 23:13:27 +0300

    refactor(error-handler): disable CustomAPIError handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,13 +1,13 @@
-const { CustomAPIError } = require("../errors");
+// const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong try again later",
   };
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
     customError.statusCode = StatusCodes.BAD_REQUEST;
