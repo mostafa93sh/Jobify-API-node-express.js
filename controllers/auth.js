80ab0ad95a57d45fac90fd2d197523436295baf5
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-24 14:32:29 +0300

    feat(auth): implement user login functionality
    - Added password comparison logic
    - Implemented credential validation
    - Added JWT token generation on successful login
    - Integrated error handling for invalid credentials
    - Added status code for OK response

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const StatusCode = require("http-status-codes");
 
 const register = async (req, res) => {
   const user = await User.create(req.body);
@@ -8,7 +10,21 @@ const register = async (req, res) => {
   res.status(201).json({ username: user.name, token });
 };
 const login = async (req, res) => {
-  res.json("login work");
+  const { email, password } = req.body;
+  console.log(req.body);
+  if (!email || !password) {
+    throw new BadRequestError("you should provide email and password");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("invalid credentials");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("invalid credentials");
+  }
+  const token = user.createJWT();
+  res.status(StatusCode.StatusCodes.OK).json({ username: user.name, token });
 };
 
 module.exports = {
