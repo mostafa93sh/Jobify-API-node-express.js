9a802ff49085d18ef43a07b710da107520d5e80e
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-24 07:16:01 +0300

    build: upgrade mongoose to v9 and add bcrypt
    - Upgrade Mongoose from v5 to v9
    - Refactor database connection setup
    - Add `bcrypt` package as a new dependency
    -

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
-const connectDB = require("./db/connect");
+const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
 
@@ -17,17 +17,17 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("jobs api");
 });
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/auth/", authRouter);
+app.use("/api/v1/jobs/", jobsRouter);
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+// app.use(notFoundMiddleware);
+// app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    connectDB(`${process.env.MONGO_URI}`);
+    mongoose.connect(`${process.env.MONGO_URI}`);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`),
     );
