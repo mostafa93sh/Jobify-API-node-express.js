1e750bd7659c19fa2655dab0b7c2b6a20bbe6bd3
mostafa93sh <mostafa.khalel.93@gmail.com>
2026-04-24 07:12:58 +0300

    feat(auth): implement user registration
    - Implement password hashing with bcrypt
    - Store new user data in the database
    - Handle user creation and response

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,15 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
 const register = async (req, res) => {
-  res.json("register work");
+  const { name, email, password } = req.body;
+  console.log(req.body);
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const tempUser = { name, email, password: hashedPassword };
+  const user = await User.create({ ...tempUser });
+
+  res.status(201).json({ user });
 };
 const login = async (req, res) => {
   res.json("login work");
